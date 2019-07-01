0a63a18160e895ebbf48f393c18d435d9116215e
Ivan Shepler <true.man.mow@gmail.com>
2019-07-01 12:22:25 +0300

    Initial Commit

---
+++ margo-telegram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  margo-telegram
+//
+//  Created by Ivan Shpileuski on 7/1/19.
+//  Copyright © 2019 Ivan Shpileuski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
